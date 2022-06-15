0b940b2d04d326b4681dda64a292617b5a8a88c7
Oleg Zheleznov <oleg-zheleznov@mail.ru>
2022-06-15 17:42:55 +0300

    Fix test

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,10 +2,5 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
-        if (first > second) {
-            System.out.println(first);
-        } else {
-            System.out.println(second);
-        }
     }
 }
