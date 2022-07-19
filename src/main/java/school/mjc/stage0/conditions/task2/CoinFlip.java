82c19c3940fdc42bd7dbbae4b2f6344496899b6b
NANO <File changed>
2022-07-19 23:28:32 +0600

    1 submission

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -2,6 +2,10 @@ package school.mjc.stage0.conditions.task2;
 
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
-
+if(from1UpTo1000 < 500){
+    System.out.println("Eagle");
+     }else{
+    System.out.println("Tail");
+      }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if(first > second){
+            System.out.println("first");
+        }else if(second > first){
+            System.out.println("second");
+        }else{
+            System.out.println("first" + "second");
+        }
     }
 }
